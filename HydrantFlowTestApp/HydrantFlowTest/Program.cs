f5370a9448238988f8003b3d1d652702b64b0d95
cbasford <cbasford@nnva.gov>
2022-01-05 10:06:53 -0500

    initial commit

---
+++ HydrantFlowTestApp/HydrantFlowTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrantFlowTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My new app");
+        }
+    }
+}
