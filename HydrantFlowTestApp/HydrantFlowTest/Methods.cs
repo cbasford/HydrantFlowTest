7efeb0f04f05af83e9656d64bfa7f93d29e1814c
cbasford <cbasford@nnva.gov>
2022-01-08 11:11:07 -0500

    Created MS Word template file.  Added classes for flow test information.  Write flow test information to fields in Word template.

---
+++ HydrantFlowTestApp/HydrantFlowTest/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HydrantFlowLibrary.Models;
+
+namespace HydrantFlowTest
+{
+    public class Methods
+    {
+        public static void AddData(FlowTestModel flowTest, FlowTestDataModel flowtestData)
+        {
+            // Add flow test
+            flowTest.Id = 1;
+            flowTest.TestDate = System.DateTime.Now;
+            flowTest.TestName = "100 Main St";
+
+            // Add flow test data
+            flowtestData.Id = 1;
+            flowtestData.FlowTestId = 1;
+            flowtestData.Asset = "hyd";
+            flowtestData.AssetId = 100;
+            flowtestData.AssetRole = "flow";
+            flowtestData.StaticPsi = 50;
+            flowtestData.TestPsi = 40;
+            flowtestData.Flow = 1200;
+
+            flowtestData.Id = 2;
+            flowtestData.FlowTestId = 1;
+            flowtestData.Asset = "hyd";
+            flowtestData.AssetId = 200;
+            flowtestData.AssetRole = "res";
+            flowtestData.StaticPsi = 50;
+            flowtestData.TestPsi = 40;
+
+
+        }
+    }
+}
---
+++ HydrantFlowTestApp/HydrantFlowTest/Models/FlowTestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrantFlowLibrary.Models
+{
+    public class FlowTestModel
+    {
+        public int Id { get; set; }
+        public string TestName { get; set; }
+        public DateTime TestDate { get; set; }
+        public bool TestCurrent { get; set; }
+        public List<FlowTestDataModel> TestData { get; set; } = new List<FlowTestDataModel>();
+        //public List<RequestModel> Requests { get; set; } = new List<RequestModel>();
+    }
+}
